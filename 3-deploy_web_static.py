0357859cfc84cecca5c0b6fdeb43c4809774063b
Nathaniel81 <nathanield356@gmail.com>
2022-12-07 23:20:02 +0300

    Deploy web static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,8 +2,8 @@
 # Fabfile to create and distribute an archive to a web server.
 import os.path
 from datetime import datetime
-from fabric.api import env
 from fabric.api import local
+from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
